00f44259e7d8d5ce705adb4c4ede355e503d8fa5
RG1ee <niwows@yandex.ru>
2022-08-16 01:51:57 +0300

    db -> .gitignore, add DEBUG in .env

--- src/task_book/settings.py
+++ src/task_book/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
+
 SECRET_KEY = os.getenv('SECRET_KEY')
+BOOL_DEBUG = bool(os.getenv('DEBUG'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=BOOL_DEBUG
 
 ALLOWED_HOSTS = []
 
