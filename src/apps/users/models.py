747bfeb0b0c8a59a66a262ecda529816ebdceaea
RG1ee <niwows@yandex.ru>
2022-09-02 00:52:12 +0300

    delete user migration and refactoring User model

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 from src.apps.users.consts import Role
-from src.apps.achievements.models import Achievement
+# from src.apps.achievements.models import Achievement
 
 
 class User(AbstractUser):
@@ -12,7 +12,7 @@ class User(AbstractUser):
         default=Role.STUDENT
     )
     github_link = models.URLField(verbose_name='GitHub Profile', null=True, blank=True)
-    # achievements = models.ManyToManyField(Achievement)
+    #achievements = models.ManyToManyField(Achievement) # TODO: probably will be moved to Student Profile model
 
     def __str__(self):
         return self.username
