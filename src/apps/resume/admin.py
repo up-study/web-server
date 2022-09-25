3f529b3012df5055d3d7fd42061988bf18e57e67
RG1ee <niwows@yandex.ru>
2022-09-25 12:41:12 +0300

    create table `resume` for user

---
+++ src/apps/resume/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from src.apps.resume.models import Language, Resume, ProgramminngLanguage
+
+
+@admin.register(Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProgramminngLanguage)
+class ProgrammingLanguageAdmin(admin.ModelAdmin):
+    pass
