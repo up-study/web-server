8be926a1f4ab79d4cbee124c00c79b6d80dcf6b5
Ruslan Korneev <kornerus@fjfi.cvut.cz>
2022-10-09 12:28:41 +0200

    add sentry

--- src/upstudy/settings.py
+++ src/upstudy/settings.py
@@ -142,6 +142,18 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": True,
 }
 
+# Sentry
+if SENTRY_DSN := os.environ.get("SENTRY_DSN", ""):
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=True,
+    )
+
 # Redis
 REDIS_HOST = os.environ.get("REDIS_HOST", "0.0.0.0")
 REDIS_PORT = os.environ.get("REDIS_PORT", "6379")
