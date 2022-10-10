4b71669e272c49d25395949047c3349d0afc4cf0
RG1ee <niwows@yandex.ru>
2022-10-10 20:22:04 +0300

    add insensitive case filtering

--- src/apps/resume/api/filters.py
+++ src/apps/resume/api/filters.py
@@ -4,15 +4,15 @@ from src.apps.resume.models import Resume
 
 class ResumeFilter(FilterSet):
     programming_language = CharFilter(
-        "programming_language__name", lookup_expr="startswith"
+        "programming_language__name", lookup_expr="istartswith"
     )
-    language = CharFilter("language__name", lookup_expr="startswith")
+    language = CharFilter("language__name", lookup_expr="istartswith")
     programming_language_code = CharFilter(
-        "programming_language__code", lookup_expr="startswith"
+        "programming_language__code", lookup_expr="istartswith"
     )
-    language_code = CharFilter("language__code", lookup_expr="startswith")
-    city = CharFilter("city__name", lookup_expr="startswith")
-    country = CharFilter("city__country__name", lookup_expr="startswith")
+    language_code = CharFilter("language__code", lookup_expr="istartswith")
+    city = CharFilter("city__name", lookup_expr="istartswith")
+    country = CharFilter("city__country__name", lookup_expr="istartswith")
 
     class Meta:
         model = Resume
