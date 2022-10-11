b6a92f0a2a200ef5f4336d18ac1a9f336387ba0c
Roman Lee (kittzy) <105285331+RG1ee@users.noreply.github.com>
2022-10-11 16:41:54 +0300

    UP-21: add city and country fields to filter (#31)

--- src/apps/resume/api/filters.py
+++ src/apps/resume/api/filters.py
@@ -1,13 +1,26 @@
-from django_filters import rest_framework as filter
+from django_filters import FilterSet, CharFilter
 from src.apps.resume.models import Resume
 
 
-class ResumeFilter(filter.FilterSet):
+class ResumeFilter(FilterSet):
+    programming_language = CharFilter(
+        "programming_language__name", lookup_expr="istartswith"
+    )
+    language = CharFilter("language__name", lookup_expr="istartswith")
+    programming_language_code = CharFilter(
+        "programming_language__code", lookup_expr="istartswith"
+    )
+    language_code = CharFilter("language__code", lookup_expr="istartswith")
+    city = CharFilter("city__name", lookup_expr="istartswith")
+    country = CharFilter("city__country__name", lookup_expr="istartswith")
+
     class Meta:
         model = Resume
         fields = (
-            "programming_language__name",
-            "language__name",
-            "programming_language__code",
-            "language__code",
+            "programming_language",
+            "programming_language_code",
+            "language",
+            "language_code",
+            "city",
+            "country",
         )
