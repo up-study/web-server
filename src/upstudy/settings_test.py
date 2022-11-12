176e50ca4574bc5627300381b0cb23b5907bfba4
Ruslan Korneev <kornerus@fjfi.cvut.cz>
2022-11-12 04:49:02 +0100

    ignore f401 and f403, setup test db

--- src/upstudy/settings_test.py
+++ src/upstudy/settings_test.py
@@ -1,1 +1,9 @@
-from .settings import *
+from .settings import *  # noqa[F403,F401]
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
